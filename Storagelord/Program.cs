c7f4576393a9c4252a0203a944e8c5e9079ac1fd
Salo Lassi <s2001067@edu.bc.fi>
2022-08-26 12:36:05 +0300

    Added 2 features
    Added "Lisää uusi tuote" & "Poista tuote"

--- Storagelord/Program.cs
+++ Storagelord/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Storagelord;
+
+Console.WriteLine($"- - STORAGELORD - -\n 1 - Lisää uusi tuote\n 2 - Poista tuote\n 3 - Tulosta eri tuotteiden määrät\n 4 - Muokkaa tuotenimeä\n 0 - Lopeta sovellus");
+static bool AddTuote(int uusiID, string uusiNimi, float uusiHinta, int uusiSaldo)
+{
+    using (Varastohallinta varastohallinta = new())
+    {
+        Tuote tuote = new()
+        {
+            ID = uusiID,
+            TUOTENIMI = uusiNimi,
+            TUOTEHINTA = uusiHinta,
+            VARASTOSALDO = uusiSaldo
+        };
+        varastohallinta.tuotteet?.Add(tuote);
+        int affected = varastohallinta.SaveChanges();
+        return (affected == 1);
+    }
+}
+static int DeleteTuote(int ID)
+{
+    using (Varastohallinta varastohallinta = new())
+    {
+        Tuote tuoteToDelete = varastohallinta.tuotteet.Find(ID);
+        if (tuoteToDelete is null)
+        {
+            Console.WriteLine("Ei löydy!");
+            return 0;
+        }
+        else
+        {
+            varastohallinta.Remove(tuoteToDelete);
+            int affected = varastohallinta.SaveChanges();
+            return affected;
+        }
+    }
+}
